a716b39d8e1474bf4c68931bc555b7822ca89510
Maaroufi <34818183+Maaroufi@users.noreply.github.com>
2020-10-01 18:43:38 +0100

    Assets new

---
+++ Assets/Scripts/CamSetup.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using System.IO.Ports;
+using UnityEngine.UI;
+
+public class CamSetup : MonoBehaviour {
+
+    //la raw image sur laquel je vais rendre ma texture de webcam
+    [SerializeField] private RawImage planeToRenderTexture;
+
+    //la webcam que je choisi
+    private WebCamDevice device;
+
+    //la texture de ma webcam activée
+    //private WebCamTexture mCameraTexture = null;
+    public int test;
+
+    //if using a plane or prefab gameobject
+    public GameObject WebcamTexurePrefab;
+
+    WebCamTexture mCameraTexture;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        // for debugging purposes, prints available devices to the console
+        for (int i = 0; i < devices.Length; i++)
+        {
+            print("Webcam available: " + devices[i].name);
+        }
+
+        //mCameraTexture = new WebCamTexture(devices[test].name, 10000, 10000, 30);
+
+        
+        mCameraTexture = new WebCamTexture(devices[test].name);
+        mCameraTexture.requestedFPS = 30f;                   //Merveilleux, un float pour le FPS...
+        mCameraTexture.requestedWidth = 1920;   //int de calibration, à vous de choisir
+        mCameraTexture.requestedWidth = 1080;	//int de calibration, à vous de choisir
+        planeToRenderTexture.texture = mCameraTexture;				//RawImage d'affichage
+        mCameraTexture.Play();
+
+
+        /*WebCamTexture mCameraTexture = new WebCamTexture(devices[test].name, 1920, 1080, 30);
+        planeToRenderTexture.texture = mCameraTexture;              //RawImage d'affichage
+        mCameraTexture.Play();*/
+
+        //OnCameraDisplay();
+    }
+
+     public void OnCameraDisplay()
+    {
+        //je connais la texture qui contiendra l'image de ma webcam il est tant que je l'applique à ma Raw Image
+        //planeToRenderTexture.texture = mCameraTexture;
+
+        //if using plane
+        //WebcamTexurePrefab.GetComponent<Renderer>().material.mainTexture = mCameraTexture;
+
+        //on lance la webcam
+        //mCameraTexture.Play();
+
+    }
+
+    void Update()
+    {
+        //transform.rotation = baseRotation * Quaternion.AngleAxis(mCameraTexture.videoRotationAngle, Vector3.up);
+    }
+
+}
---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class PlayerControllerScript: MonoBehaviour
+{
+	Thread receiveThread;
+	UdpClient client;
+    IPEndPoint anyIP;
+    int port;
+
+    public GameObject glove;
+    public Camera oculusCam;
+    public Renderer rend, rendSphere;
+    public GameObject interactObject;
+    public GameObject plane;
+    public Boolean colorShift;
+
+    Vector3 point = new Vector3();
+    float x;
+    float y;
+
+    bool detected;
+
+    private void Awake()
+    {
+        // Switch to 640 x 480 full-screen
+        //Screen.SetResolution(1080, 1200, true);
+        detected = false;
+        colorShift = false;
+
+        rend = glove.GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+    void Start () 
+	{
+        rendSphere = interactObject.GetComponent<Renderer>();
+        port = 5065;
+        anyIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), port);
+        InitUDP();
+
+        //Fetch the Renderer from the GameObject
+
+
+        x = 0;
+        y = 0;
+    }
+
+	// InitUDP
+	private void InitUDP()
+	{
+		print ("UDP Initialized");
+		receiveThread = new Thread (new ThreadStart(ReceiveData)); 
+		receiveThread.IsBackground = true; 
+		receiveThread.Start (); 
+	}
+
+	// 4. Receive Data
+	private void ReceiveData()
+	{
+		client = new UdpClient (port); 
+		while (true) 
+		{
+			try
+			{
+				byte[] data = client.Receive(ref anyIP);
+
+                string text = Encoding.UTF8.GetString(data);
+                //print (">> " + text);
+                string[] coordinates = text.Split('#');
+                //print ("x coordinate ONLY = " + coordinates[0]);
+                detected = true;
+                x = float.Parse(coordinates[0]);
+                y = float.Parse(coordinates[1]);
+
+            } catch(Exception e)
+			{
+				print (e.ToString());
+			}
+		}
+	}
+
+	public void SendMessage()
+	{
+        print("hand detected");
+	}
+
+	// Check for variable value, and send message!
+	void FixedUpdate() 
+	{
+		if(detected == true)
+		{
+            SendMessage();
+
+            if(colorShift == false)
+            {
+                //rendSphere.material.shader = Shader.Find("_colorSphere");
+                rendSphere.material.SetColor("_Color", Color.blue);
+                colorShift = true;
+            } else
+            {
+                //rendSphere.material.shader = Shader.Find("_colorSphere");
+                rendSphere.material.SetColor("_Color", Color.red);
+                colorShift = false;
+            }  
+
+            //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //cube.transform.position = new Vector3(x, y, 0);
+            rend.enabled = true;
+            y = y - 416f;
+            //point = oculusCam.ScreenToWorldPoint(new Vector3(x + 150f, Math.Abs(y), oculusCam.nearClipPlane + 3f));
+            //point = oculusCam.ScreenToWorldPoint(new Vector3(x, y, interactObject.transform.position.z));
+            point = new Vector3(plane.transform.position.x, plane.transform.position.y, plane.transform.position.z - 2f);
+            
+            glove.transform.position = point;
+            detected = false;
+        } else
+        {
+            //rend.enabled = false;
+        }
+
+	}
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("Application ending after " + Time.time + " seconds");
+        receiveThread.Abort();
+        client.Close();
+    }
+}
